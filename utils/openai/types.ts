6cf5f5d484d8444ef8b0a5e57d0888131f8c5f26
127Wzc <1274925976@qq.com>
2026-03-16 00:23:59 +0800

    Revert "feat: API 不再保存历史图片base64；API支持对话历史记录条数控制"
    This reverts commit 8198d3686888e7a3b6b46430a423d0da5569815c.

--- utils/openai/types.ts
+++ utils/openai/types.ts
@@ -28,9 +28,6 @@ export type ChatGPTAPIOptions = {
 
     /** @defaultValue `1000` **/
     maxResponseTokens?: number
-    
-    /** 限制历史记录最大条数 */
-    chatgptBlockCount?: number 
 
     messageStore?: Keyv
     getMessageById?: GetMessageByIdFunction
